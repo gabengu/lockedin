d8722a0ccebfd1a91415b7b686768b2c792b479b
gabengu <gabengu@gmail.com>
2025-07-31 11:46:30 -0600

    added height and width to Image

--- Frontend/src/app/draft/[draftID]/icons.tsx
+++ Frontend/src/app/draft/[draftID]/icons.tsx
@@ -8,6 +8,8 @@ export default function Icons(props: IconsProps) {
   return (
     <Image
       alt={props.name}
+      height={100}
+      width={100}
       src={
         "https://ddragon.leagueoflegends.com/cdn/15.15.1/img/champion/" +
         props.name +
