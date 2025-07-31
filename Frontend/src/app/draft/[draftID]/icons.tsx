cee7aedf6e5f938d90e65b295bddf0f5ff19d0ba
gabengu <gabengu@gmail.com>
2025-07-31 11:30:26 -0600

    Fixed icons type

--- Frontend/src/app/draft/[draftID]/icons.tsx
+++ Frontend/src/app/draft/[draftID]/icons.tsx
@@ -1,6 +1,13 @@
-export default function Icons(props: any) {
+import Image from "next/image";
+
+type IconsProps = {
+  name: string;
+};
+
+export default function Icons(props: IconsProps) {
   return (
-    <img
+    <Image
+      alt={props.name}
       src={
         "https://ddragon.leagueoflegends.com/cdn/15.15.1/img/champion/" +
         props.name +
