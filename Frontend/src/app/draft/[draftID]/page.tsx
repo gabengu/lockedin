f62d5e18db260a14efc3d660617a2b127863355c
Jerrit Smith <126747440+Jortmcchez@users.noreply.github.com>
2025-07-30 19:05:18 -0600

    Update page.tsx

--- Frontend/src/app/draft/[draftID]/page.tsx
+++ Frontend/src/app/draft/[draftID]/page.tsx
@@ -1,7 +1,22 @@
+import champion from "./champion.json" assert { type: "json" };
+import Icons from "./icons.tsx";
 
-export default async function DraftRoom({ params }: { params: { draftID: string } }) {
-  const draftId = (await params).draftID
-  //console.log("draftId", draftId)
+const champsString = JSON.stringify(champion);
+const champsObj = JSON.parse(champsString);
+const names = Object.keys(champsObj.data);
+const iconElements = names.map((name) => {
+  return <Icons name={name} />;
+});
+export default async function DraftRoom({
+  params,
+}: {
+  params: { draftID: string };
+}) {
+  const draftId = (await params).draftID;
 
-  return <h1>Hello draft page! Link: {draftId}</h1>
+  //https://ddragon.leagueoflegends.com/cdn/15.15.1/data/en_US/champion.json
+
+  // https://ddragon.leagueoflegends.com/cdn/15.15.1/img/champion/Aatrox.png
+
+  return <main>{iconElements}</main>;
 }
