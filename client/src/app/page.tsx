48d7d9833b55aa398a414c1a2cff25b50f0d2755
Siwon <siwonkim0206@gmail.com>
2025-08-01 16:30:43 -0600

    styling

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,10 @@
-import NavBar from "@/components/navbar";
-import Image from "next/image";
+import  Image from "next/image";
+import NavBar from "./components/navbar";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-black">
-         <NavBar />
+         <NavBar></NavBar>
          <div className="flex flex-col items-center justify-center h-screen text-white">
           <div className="bg-gradient-to-r from-white/50 h-1/2 w-1/2 rounded-2xl border-1">
 
