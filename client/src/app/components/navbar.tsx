80e89c4262dd4183c963be463420b8d703d1bd42
Siwon <Siwonkim0206@gmail.com>
2025-08-02 21:27:01 -0600

    fix routing

--- client/src/app/components/navbar.tsx
+++ client/src/app/components/navbar.tsx
@@ -10,9 +10,9 @@ export default function NavBar() {
             <div className="container mx-auto flex items-center justify-between" >
                 <div className="bg-gray-600 text-white py-6 px-10 rounded">LOGO</div>
                 <ul className="absolute left-1/2  -translate-x-1/2  flex space-x-40 text-lg">
-                    <li className="cursor-pointer hover:underline" onClick={() => router.push('/')}>DRAFT TOOL</li>
+                    <li className="cursor-pointer hover:underline" onClick={() => router.push('/draft')}>DRAFT TOOL</li>
                     <li className="cursor-pointer hover:underline" onClick={() => router.push('/')}>ABOUT</li>
-                    <li className="cursor-pointer hover:underline" onClick={() => router.push('/draft')}>JOIN</li>
+                    <li className="cursor-pointer hover:underline" onClick={() => router.push('/')}>JOIN</li>
                 </ul>
             </div>
         </nav>
