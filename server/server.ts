7c91b307cdc747d5c69be249c46a7558e8790bf8
gabengu <gabengu@gmail.com>
2025-08-06 03:08:04 -0600

    added tsconfig and types for socket.io

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,11 @@
+import type { Socket } from "socket.io";
+
 const io = require("socket.io")(3001, {
     cors: {
         origin: ["http://localhost:3000"],
     },
 });
 
-io.on("connection", (socket) => {
+io.on("connection", (socket: Socket) => {
     console.log(socket.id);
 });
