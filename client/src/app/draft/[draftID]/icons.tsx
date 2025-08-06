eb4e2cd637aeb85f8cec817668d9bfdd69781369
Siwon <Siwonkim0206@gmail.com>
2025-08-06 02:32:34 -0600

    pull from main

--- client/src/app/draft/[draftID]/icons.tsx
+++ client/src/app/draft/[draftID]/icons.tsx
@@ -2,20 +2,22 @@ import Image from "next/image";
 
 type IconsProps = {
   name: string;
+  height: number;
+  width: number;
+  disable: boolean;
 };
 
 export default function Icons(props: IconsProps) {
   return (
-    <Image
-      className="hover:scale-[105%] transition-all cursor-pointer"
-      alt={props.name}
-      height={100}
-      width={100}
-      src={
-        "https://ddragon.leagueoflegends.com/cdn/15.15.1/img/champion/" +
-        props.name +
-        ".png"
-      }
-    />
+        <Image
+          alt={props.name}
+          height={props.height}
+          width={props.width}
+          src={
+            "https://ddragon.leagueoflegends.com/cdn/15.15.1/img/champion/" +
+            props.name +
+            ".png"
+          }
+        />
   );
 }
--- client/src/app/draft/[draftID]/page.tsx
+++ client/src/app/draft/[draftID]/page.tsx
@@ -1,11 +1,27 @@
 "use client";
 import { useState, useEffect } from "react";
 import Icons from "./icons.tsx";
+import SplashArt from "./splashArt.tsx";
+import { draftOrder } from "./draftOrder.tsx"; 
 
 export default function DraftRoom() {
-  const [champions, setChampions] = useState({});
+  const [allChampions, setAllChampions] = useState<string[]>([]);
+  const [filterChampions, setFilterChampions] = useState<string[]>([]);
+  const [draftPhase, setDraftPhase] = useState<"ban" | "pick">("ban");
+  const [activeSide, setActiveSide] = useState<"blue" | "red">("blue");
+
+  const [selectedBan, setSelectedBan] = useState<string | null>(null);
+  const [selectedPick, setSelectedPick] = useState<string | null>(null);
+
+  const [blueTeamPicks, setBlueTeamPicks] = useState(new Array(5).fill(null));
+  const [redTeamPicks, setRedTeamPicks] = useState(new Array(5).fill(null));
+  const [blueTeamBans, setBlueTeamBans] = useState(new Array(5).fill(null));
+  const [redTeamBans, setRedTeamBans] = useState(new Array(5).fill(null));
+  
+  
+  
   useEffect(() => {
-    async function getChampions() {
+    async function getAllChampions() {
       const response = await fetch(
         "https://ddragon.leagueoflegends.com/cdn/13.20.1/data/en_US/champion.json"
       );
@@ -13,14 +29,90 @@ export default function DraftRoom() {
         throw new Error("Failed to fetch champions");
       }
       const data = await response.json();
-      setChampions(data.data);
+      setAllChampions(Object.keys(data.data));
+      setFilterChampions(Object.keys(data.data));
+      console.log(data.data)
     }
-    getChampions();
+    getAllChampions();
   }, []);
-  const iconElements = Object.keys(champions).map(
+
+  const renderIcons = () => {
+    return filterChampions.map(
     (name: string, index: number) => {
-      return <Icons key={index} name={name} />;
+      return(
+      <div 
+      key={name}
+      className ="m-1 rounded hover:scale-[105%] transition-all cursor-pointer" onClick={() => handleChampionBan(name, activeSide)}>
+        <Icons key={index} name={name} disable={false} height={75} width={75}/>
+      </div>)
+      }
+    );
+  }
+
+  const handleChampionBan =(champion: string, team: "blue" | "red") => {
+    if (team === "blue") {  
+
     }
-  );
-  return <main className="flex flex-wrap ">{iconElements}</main>;
+  }
+
+
+  
+  const splashArtElements = Object.keys(allChampions).map(
+    (name: string, index: number) => {
+      return <div></div>
+
+    })
+
+
+  return (
+    <div className="flex flex-col bg-black min-h-screen">
+      <div className="w-full bg-gray-500 border-black border-y-2 h-[100px] flex flex-row justify-between items-center px-10">
+      <h1>
+        TEAM 1
+      </h1>
+      <h1 className="text-2xl">
+        0 - 0
+      </h1>
+      <h1>
+        TEAM 2
+      </h1>
+      </div>
+      <div className="flex flex-row justify-between mt-20">
+        <div className=" flex flex-col w-[300px]">
+          <div className=" border-t-2 border-r-2 border-gray-600 flex-1 ">
+            <SplashArt name={"Lucian"} />
+            </div>
+          <div className=" border-t-2 border-r-2 border-gray-600 flex-1 "></div>
+          <div className=" border-t-2 border-r-2 border-gray-600 flex-1 "></div>
+          <div className=" border-t-2 border-r-2 border-gray-600 flex-1 "></div>
+          <div className=" border-t-2 border-r-2 border-gray-600 flex-1 "></div>
+        <div className="flex flex-row">
+            <div className="border-t-2 border-r-2 border-b-2 border-gray-600 w-[60px] h-[60px]"></div>
+            <div className="border-t-2 border-r-2 border-b-2 border-gray-600 w-[60px] h-[60px]"></div>
+            <div className="border-t-2 border-r-2 border-b-2 border-gray-600 w-[60px] h-[60px]"></div>
+            <div className="border-t-2 border-r-2 border-b-2 border-gray-600 w-[60px] h-[60px]"></div>
+            <div className="border-t-2 border-r-2 border-b-2 border-gray-600 w-[60px] h-[60px]"></div>
+          </div>
+        </div>
+
+        <div className=" flex flex-wrap overflow-y-auto w-[800px] h-[calc(100vh-180px)] items-center justify-center hide-scrollbar">{renderIcons()}</div>
+
+        <div className=" flex flex-col w-[300px]">
+          <div className=" border-t-2 border-l-2 border-gray-600 flex-1 "></div>
+          <div className=" border-t-2 border-l-2 border-gray-600 flex-1 "></div>
+          <div className=" border-t-2 border-l-2 border-gray-600 flex-1 "></div>
+          <div className=" border-t-2 border-l-2 border-gray-600 flex-1 "></div>
+          <div className=" border-t-2 border-l-2 border-gray-600 flex-1 "></div>
+          <div className="flex flex-row">
+            <div className="border-t-2 border-l-2 border-b-2 border-gray-600 w-[60px] h-[60px]"></div>
+            <div className="border-t-2 border-l-2 border-b-2 border-gray-600 w-[60px] h-[60px]"></div>
+            <div className="border-t-2 border-l-2 border-b-2 border-gray-600 w-[60px] h-[60px]"></div>
+            <div className="border-t-2 border-l-2 border-b-2 border-gray-600 w-[60px] h-[60px]"></div>
+            <div className="border-t-2 border-l-2 border-b-2 border-gray-600 w-[60px] h-[60px]"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  )
 }
