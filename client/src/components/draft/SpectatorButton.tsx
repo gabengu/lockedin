33ce0a8dd90eaedcb49dcf75df795f3f374207e9
Jerrit Smith <126747440+Jortmcchez@users.noreply.github.com>
2025-09-10 13:28:54 -0600

    more styling

--- client/src/components/draft/SpectatorButton.tsx
+++ client/src/components/draft/SpectatorButton.tsx
@@ -7,9 +7,13 @@ type specButtonProps = {
 export default function SpectatorButton(props: specButtonProps) {
     return (
         <>
-            <button className="text-2xl bg-green-500 px-20 py-3" onClick={props.function}>
-                Spectator
-            </button>
+            <div className="rounded shadow-2xl/50 hover:bg-green-400 text-2xl font-bold bg-green-500 px-20 py-3 cursor-pointer" onClick={props.function}>
+                <div className="flex flex-col items-center">
+                    <div className="flex flex-col justify-center">
+                        Spectator
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
